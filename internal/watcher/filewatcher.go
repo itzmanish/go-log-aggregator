37266e55e2f3e1a4edc1f9fa77a21d3932d4be75
Manish <itzmanish108@gmail.com>
2021-06-18 15:43:21 +0530

    Added: filewatcher

---
+++ internal/watcher/filewatcher.go
@@ -0,0 +1,56 @@
+package watcher
+
+import (
+	"time"
+
+	"github.com/hpcloud/tail"
+	"github.com/itzmanish/go-logent/config"
+	"github.com/itzmanish/go-logent/internal/logger"
+)
+
+type Result struct {
+	Name string `json:"name"`
+	// TODO: right now tags should be sent every time will fix it later for performance improvements
+	Tags      []map[string]interface{} `json:"tags"`
+	Logs      string                   `json:"log"`
+	Timestamp time.Time                `json:"time"`
+}
+
+type fileWatcher struct {
+	files  config.Watchers
+	result chan Result
+}
+
+func NewFileWatcher(files config.Watchers) Watcher {
+	return &fileWatcher{
+		files:  files,
+		result: make(chan Result),
+	}
+}
+
+func (fw *fileWatcher) Watch() {
+	for _, file := range fw.files {
+		go func(file config.Watcher) {
+			t, err := tail.TailFile(file.Watch, tail.Config{Follow: true})
+			if err != nil {
+				logger.Error(err)
+			}
+			for line := range t.Lines {
+				res := Result{Name: file.Watch, Tags: file.Tags, Logs: line.Text, Timestamp: line.Time}
+				fw.result <- res
+			}
+		}(file)
+	}
+}
+
+func (fw *fileWatcher) Result() chan Result {
+	return fw.result
+}
+
+func (fw *fileWatcher) Close() {
+	close(fw.result)
+}
+
+func (fw *fileWatcher) String() string {
+	return "File watcher"
+}
--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -1,31 +1,8 @@
 package watcher
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/hpcloud/tail"
-	"github.com/itzmanish/go-logent/pkg/config"
-	"github.com/itzmanish/go-logent/pkg/logger"
-)
-
 type Watcher interface {
 	Watch()
-}
-
-type watch struct {
-	file os.File
-}
-
-func Watch() {
-	watchers := []config.Watcher{}
-	err := config.Scan("watchers", &watchers)
-	if err != nil {
-		logger.Log(err)
-	}
-	t, err := tail.TailFile(watchers[0].Watch, tail.Config{Follow: true})
-	logger.Log(err)
-	for line := range t.Lines {
-		fmt.Println(line.Text)
-	}
+	Result() chan Result
+	Close()
+	String() string
 }
