b998dc37eddae17b492fb4b824e113d4a8a7c479
Manish <itzmanish108@gmail.com>
2021-06-23 16:48:10 +0530

    Add: [Transport] Log Body data type

--- internal/transport/transport.go
+++ internal/transport/transport.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+// LogBody describe body of a single packets.
+// It contains the name of log with log to store and tags
+// along with timestamp field which contains the time of creation of log
+type LogBody struct {
+	Name      string                   `json:"name"`
+	Log       string                   `json:"log"`
+	Tags      []map[string]interface{} `json:"tags"`
+	Timestamp time.Time                `json:"timestamp"`
+}
+
 // Packet is type of every packet that gets transported from client to server to client.
 type Packet struct {
 	// ID of packet
@@ -12,7 +22,9 @@ type Packet struct {
 	// Cmd is command containing the packets
 	Cmd string `json:"cmd"`
 	// Body is body of associated with command
-	Body interface{} `json:"body"`
+	Body *LogBody `json:"body"`
+	// Ack acknowledges for sent request
+	Ack bool
 	// Timestamp is time of packet creation.
 	Timestamp time.Time `json:"timestamp"`
 }
