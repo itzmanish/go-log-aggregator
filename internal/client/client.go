17224f6957103cc1a8f5ebb13f4ef542ec5fba54
Manish <itzmanish108@gmail.com>
2021-06-23 17:29:11 +0530

    Fix: [Client] read as independently for every request from server.

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,15 +1,22 @@
 package client
 
+import "github.com/itzmanish/go-loganalyzer/internal/transport"
+
 type Client interface {
 	Init(opts ...Option) error
 	Options() Options
+	Out() chan *transport.Packet
 	Send(data interface{}) error
 	Recv(out interface{}) error
+	Read() error
+	Close() error
 	String() string
 }
 
 func NewClient(opts ...Option) (Client, error) {
-	t := &tcpClient{}
+	t := &tcpClient{
+		out: make(chan *transport.Packet),
+	}
 	err := t.Init(opts...)
 	if err != nil {
 		return nil, err
--- internal/client/tcp.go
+++ internal/client/tcp.go
@@ -2,13 +2,17 @@ package client
 
 import (
 	"encoding/json"
+	"io"
 	"net"
 	"time"
+
+	"github.com/itzmanish/go-loganalyzer/internal/transport"
 )
 
 type tcpClient struct {
 	opts Options
 	conn net.Conn
+	out  chan *transport.Packet
 }
 
 func (t *tcpClient) Init(opts ...Option) error {
@@ -17,11 +21,16 @@ func (t *tcpClient) Init(opts ...Option) error {
 	}
 	conn, err := net.Dial("tcp", t.opts.Address)
 	t.conn = conn
+	go t.Read()
 	return err
 }
 
 func (t *tcpClient) Options() Options {
 	return t.opts
+}
+
+func (t *tcpClient) Out() chan *transport.Packet {
+	return t.out
 }
 
 func (t *tcpClient) Send(data interface{}) error {
@@ -36,11 +45,35 @@ func (t *tcpClient) Send(data interface{}) error {
 	return err
 }
 
+// Recv is for recieving however it is not working
+// Do not use this for now. Will fix it later.
 func (t *tcpClient) Recv(out interface{}) error {
 	if t.opts.Timeout != 0 {
 		t.conn.SetReadDeadline(time.Now().Add(t.opts.Timeout))
 	}
 	return json.NewDecoder(t.conn).Decode(&out)
+}
+
+func (t *tcpClient) Read() error {
+	decoder := json.NewDecoder(t.conn)
+	for {
+		var msg transport.Packet
+		err := decoder.Decode(&msg)
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		t.out <- &msg
+	}
+}
+
+func (t *tcpClient) Close() error {
+	close(t.out)
+	return t.conn.Close()
 }
 
 func (t *tcpClient) String() string {
