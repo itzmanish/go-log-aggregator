563ee5da7920049a6aa1f87ad5cc303e295027a5
Manish <itzmanish108@gmail.com>
2021-06-25 18:11:32 +0530

    just stop server don't check for err on stopping.

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -39,8 +39,7 @@ func TestTCPServer(t *testing.T) {
 		err = json.NewEncoder(conn).Encode(&codec.Packet{ID: uuid.New(), Cmd: "log"})
 		assert.Nil(t, err)
 
-		err = server.Stop()
-		assert.Nil(t, err)
+		server.Stop()
 	}()
 	assert.Equal(t, "TCP server", server.String())
 
