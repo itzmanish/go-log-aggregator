3c1b21bf129b698018f5db7af6a8fd82b0be81c6
Manish <itzmanish108@gmail.com>
2021-06-30 13:13:42 +0530

    Fix queue.

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -17,6 +17,7 @@ type Queue interface {
 }
 
 type memQueue struct {
+	sync.RWMutex
 	queue    sync.Map
 	length   int
 	interval time.Duration
@@ -26,35 +27,46 @@ type memQueue struct {
 func (mq *memQueue) handle() int {
 	for {
 		<-time.After(mq.interval)
-		logger.Debug("Queue [status]Total: ", mq.length)
-		go func() {
-			mq.queue.Range(func(key, value interface{}) bool {
-				logger.Info(key, value)
-				err := mq.client.Send(value)
-				if err == nil {
-					mq.Pop(key)
-				}
-				return true
-			})
-		}()
+		logger.Debug("Queue [status]Total: ", mq.Length())
+		if mq.Length() > 0 {
+			go func() {
+				mq.queue.Range(func(key, value interface{}) bool {
+					logger.Info(key, value)
+					err := mq.client.Send(value)
+					if err == nil {
+						mq.Pop(key)
+					}
+					return true
+				})
+			}()
+		}
 	}
 }
 
 func (mq *memQueue) Length() int {
+	mq.RLock()
+	defer mq.RUnlock()
 	return mq.length
 }
 
 func (mq *memQueue) Push(data interface{}) {
+	mq.Lock()
 	mq.length++
+	mq.Unlock()
 	mq.queue.Store(mq.length, data)
 }
 
 func (mq *memQueue) Pop(key interface{}) {
-	mq.queue.Delete(key)
-	if mq.length == 0 {
+	if mq.Length() == 0 {
 		return
 	}
-	mq.length--
+	_, ok := mq.Get(key)
+	if ok {
+		mq.queue.Delete(key)
+		mq.Lock()
+		mq.length--
+		mq.Unlock()
+	}
 }
 
 func (mq *memQueue) Get(k interface{}) (interface{}, bool) {
